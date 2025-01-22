7111ef9d30d7315820a5dd4abc4828850e24f9b6
Shuqi5588 <peeshuqi@gmail.com>
2025-01-22 09:27:37 -0500

    Update project content

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/learning_react_package_JSON",
+  base: '/learning_react_package_JSON/',
   plugins: [react()],
 })
